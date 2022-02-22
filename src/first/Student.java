b04da0b5097cb70e68817fef3d02bcae1128507a
1981agbash <masterofficial81@gmail.com>
2022-02-21 20:04:15 -0500

    construcor added and getter and setter has been added to the files Student.java

--- src/first/Student.java
+++ src/first/Student.java
@@ -6,6 +6,55 @@ public class Student {
 	private String classes;
 	
 	
+	public Student() {
+		super();
+	}
+
+
+	public Student(String name, int age, String classes) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.classes = classes;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public String getClasses() {
+		return classes;
+	}
+
+
+	public void setClasses(String classes) {
+		this.classes = classes;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", classes=" + classes + "]";
+	}
+
+
 	public static void main(String[] args) {
 		
 
